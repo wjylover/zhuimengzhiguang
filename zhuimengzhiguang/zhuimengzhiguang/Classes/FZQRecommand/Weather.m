4939956dc310ffa379800a7d3c9de7423931a75c
wjylover <lanou3g@lanou3gdeMac-mini.local>
2015-11-16 16:31:44 +0800

    11 16 fzq3

---
+++ zhuimengzhiguang/zhuimengzhiguang/Classes/FZQRecommand/Weather.m
@@ -0,0 +1,25 @@
+//
+//  Weather.m
+//  zhuimengzhiguang
+//
+//  Created by lanou3g on 15/11/16.
+//  Copyright © 2015年 王建业. All rights reserved.
+//
+
+#import "Weather.h"
+
+@implementation Weather
+
+
+-(void)setValue:(id)value forUndefinedKey:(NSString *)key{
+    if ([key isEqualToString:@"id"]) {
+        NSLog(@"%@",key);
+    }
+}
+
+
+- (NSString *)description
+{
+    return [NSString stringWithFormat:@"data=%@", _data];
+}
+@end
